5d0827daf03bb69a8cccaee6f9e6d902f6fbd029
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-06-01 14:03:43 +0200

    Fix database name typo in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -117,7 +117,7 @@ export default defineNuxtConfig({
           d1_databases: [
             {
               binding: 'DB',
-              database_name: 'launchpand',
+              database_name: 'launchpad',
               database_id: 'a92127c1-aaa3-4753-82ba-ea59fa9e7140'
             }
           ],
