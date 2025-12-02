2480568e1f8ade42b523a6c8b7b93a68900951ae
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-12-02 09:40:51 +0100

    chore: remove unused Google Fonts link from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,16 +11,6 @@ export default defineNuxtConfig({
             enabled: true
         }
     },
-    app: {
-        head: {
-            link: [
-                {
-                    rel: 'stylesheet',
-                    href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'
-                }
-            ]
-        }
-    },
     schemaOrg: {
         identity: defineOrganization({
             name: 'OneLiteFeather Network',
