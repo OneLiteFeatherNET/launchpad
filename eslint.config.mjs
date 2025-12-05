6b2d1b9be7a222ad2f0605b37dbcba828ee8b831
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-12-05 10:48:54 +0100

    chore: tighten eslint formatting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,24 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        tabWidth: 2,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true
+      }
+    ],
+    'object-curly-newline': ['error', { multiline: true, consistent: true }],
+    'array-element-newline': ['error', { multiline: true, minItems: 3 }],
+    'operator-linebreak': ['error', 'before'],
+    'implicit-arrow-linebreak': ['error', 'beside'],
+    'function-paren-newline': ['error', 'multiline'],
+    'no-trailing-spaces': 'error'
+  }
+})
